fff172ed76930fc295b6c40d5c817c67228577c7
arete89 <75988771+arete89@users.noreply.github.com>
2021-02-02 00:23:21 +0900

    Update ESG_Data_Finance.py

--- ESG_Data_Finance.py
+++ ESG_Data_Finance.py
@@ -1,9 +1,13 @@
 import yfinance as yf
 import pandas as pd
 import os
+
+# any stock ticker
 tesla = "TSLA"
 tesla_y = yf.Ticker(tesla)
 esg_data = pd.DataFrame.transpose(tesla_y.sustainability)
 esg_data['company_ticker'] = str(tesla_y.ticker)
-os.chdir("/Users/ronaldkim/Downloads")
+
+# location to export the spreadsheet
+os.chdir("/Users/ ")
 esg_data.to_csv('tesla_sustainability_scores.csv', encoding='utf-8')
